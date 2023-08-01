a6880267b9994d4d3ab68e724446cd5e219d6e43
Jocelyn Wensloff <jewensloff@gmail.com>
2023-08-01 16:31:46 -0600

    feat: Add a successful GET request to log the data to the console

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,18 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 
-
+import { createFetchRequests } from './apiCalls';
 console.log('This is the JavaScript entry file - your code begins here.');
+
+let masterData = {}
+
+window.addEventListener('load', () => {
+  Promise.all(createFetchRequests()).then((promiseArray) => {
+    masterData.travelers = promiseArray[0]
+    masterData.trips = promiseArray[1]
+    masterData.destinations = promiseArray[2]
+    console.log('masterData', masterData);
+  });
+});
